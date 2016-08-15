0b526432a1c71f268f037ae75df881d0cbdb673e
Eric J. Ma <ericmajinglong@gmail.com>
2016-08-15 11:14:10 -0400

    used symlinks for build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 pandoc 01-research-statement.md \
   -o "01-research-statement.pdf" \
   -V "geometry:left=0.5in, right=0.5in, bottom=1in, top=0.5in" \
-  --latex-engine="/Library/TeX/texbin/pdflatex" \
-  --filter="/Users/ericmjl/anaconda/envs/writing/bin/pandoc-fignos" \
-  --filter="/Users/ericmjl/anaconda/envs/writing/bin/pandoc-citeproc" \
+  --latex-engine="pdflatex" \
+  --filter="pandoc-fignos" \
+  --filter="pandoc-citeproc" \
   --bibliography="papers-library.bib"
