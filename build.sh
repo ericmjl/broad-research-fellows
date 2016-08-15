6dc8153e813e6cae315cba7f57dc911dd2b8917d
Eric J. Ma <ericmajinglong@gmail.com>
2016-08-15 10:46:27 -0400

    fixed build script, fixed figure numbering

--- build.sh
+++ build.sh
@@ -5,5 +5,3 @@ pandoc 01-research-statement.md \
   --filter="/Users/ericmjl/anaconda/bin/pandoc-fignos" \
   --filter="/usr/local/bin/pandoc-citeproc" \
   --bibliography="../references-master/papers-library.bib"
-
-# pandoc 02-cv.md -o "02-cv.pdf" -V "geometry:left=0.5in, right=0.5in, bottom=1in, top=0.5in" --latex-engine="/Library/TeX/texbin/xelatex"  --bibliography="/Users/ericmjl/Documents/github/writing/references-master/papers-library.bib"
