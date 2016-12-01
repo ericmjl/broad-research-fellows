1a73aa11aeec36fcf6420d2e551c41ecfd0115ed
Eric J. Ma <ericmajinglong@gmail.com>
2016-12-01 10:27:09 -0500

    changed build script to upload directly to my website

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 pandoc 01-research-statement.md \
-  -o "01-research-statement.pdf" \
-  -V "geometry:left=0.5in, right=0.5in, bottom=1in, top=0.5in" \
-  --latex-engine="pdflatex" \
-  --filter="pandoc-fignos" \
-  --filter="pandoc-citeproc" \
-  --bibliography="papers-library.bib"
+  -o 01-research-statement.pdf \
+  -V "geometry:left=0.5in, right=0.5in, bottom=0.75in, top=0.5in" \
+  --latex-engine=pdflatex \
+  --filter=pandoc-fignos \
+  --filter=pandoc-citeproc \
+  --bibliography=papers-library.bib
 
-open 01-research-statement.pdf
+
+# open 01-research-statement.pdf
+
+scp ./01-research-statement.pdf doroot:/var/www/html/cv/broad-fellows.pdf
