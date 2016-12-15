bf506b685d08ac2aac71f4847d325fbfdacc817a
Eric J. Ma <ericmajinglong@gmail.com>
2016-12-15 08:35:53 -0500

    made everything in repo self-contained

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+# Copy stuff into local directory as needed.
+cp ../references-master/papers-library.bib .
+cp ../styles/pnas.csl .
+
+# # Build PDF version
 # pandoc 01-research-statement.md \
 #   -o 01-research-statement.pdf \
 #   -V "geometry:left=0.5in, right=0.5in, bottom=0.75in, top=0.5in" \
@@ -5,9 +10,6 @@
 #   --filter=pandoc-fignos \
 #   --filter=pandoc-citeproc \
 #   --bibliography=papers-library.bib
-
-
-# open 01-research-statement.pdf
 
 # scp ./01-research-statement.pdf doroot:/var/www/html/cv/broad-fellows.pdf
 
