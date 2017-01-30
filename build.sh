1c55f722d226ebd73b5b1834605c129b8e274cec
Eric J. Ma <ericmajinglong@gmail.com>
2017-01-30 10:09:43 -0500

    bulk update

--- build.sh
+++ build.sh
@@ -2,16 +2,16 @@
 cp ../references-master/papers-library.bib .
 cp ../styles/pnas.csl .
 
-# # Build PDF version
-# pandoc 01-research-statement.md \
-#   -o 01-research-statement.pdf \
-#   -V "geometry:left=0.5in, right=0.5in, bottom=0.75in, top=0.5in" \
-#   --latex-engine=pdflatex \
-#   --filter=pandoc-fignos \
-#   --filter=pandoc-citeproc \
-#   --bibliography=papers-library.bib
+# Build PDF version
+pandoc 01-research-statement.md \
+  -o 01-research-statement.pdf \
+  -V "geometry:left=0.5in, right=0.5in, bottom=0.75in, top=0.5in" \
+  --latex-engine=pdflatex \
+  --filter=pandoc-fignos \
+  --filter=pandoc-citeproc \
+  --bibliography=papers-library.bib
 
-# scp ./01-research-statement.pdf doroot:/var/www/html/cv/broad-fellows.pdf
+scp ./01-research-statement.pdf doroot:/var/www/html/cv/broad-fellows.pdf
 
 pandoc 01-research-statement.md \
     -o index.html \
