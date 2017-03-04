ed1feba69430e9712a547ef4838b77bc30734ac6
Eric Ma <ericmajinglong@gmail.com>
2017-03-04 18:18:30 -0500

    re-created figures using jpg

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 cp ../references-master/papers-library.bib .
 cp ../styles/pnas.csl .
 
+# Make figures
+python convert_images.py
+
 # Build PDF version
 pandoc 01-research-statement.md \
   -o 01-research-statement.pdf \
@@ -11,8 +14,10 @@ pandoc 01-research-statement.md \
   --filter=pandoc-citeproc \
   --bibliography=papers-library.bib
 
+# Copy to remote server
 scp ./01-research-statement.pdf doroot:/var/www/html/cv/broad-fellows.pdf
 
+# Make HTML version
 pandoc 01-research-statement.md \
     -o index.html \
     --filter=pandoc-fignos \
@@ -21,3 +26,7 @@ pandoc 01-research-statement.md \
     -c template/notes.css \
     -H template/header.html \
     --template template/template.html
+
+git add .
+git commit
+git push
